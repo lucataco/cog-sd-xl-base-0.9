256a41598e477e414d6058a751d1d19d1022b75a
Luis C <me@lucataco.com>
2023-07-13 01:05:00 -0400

    Add torch v2 and seed to image prompt

--- predict.py
+++ predict.py
@@ -3,6 +3,7 @@ from diffusers import DiffusionPipeline
 import tempfile
 import torch
 import gc
+import os
 
 MODEL_NAME = "stabilityai/stable-diffusion-xl-base-0.9"
 MODEL_CACHE = "cache"
@@ -18,21 +19,29 @@ class Predictor(BasePredictor):
             cache_dir=MODEL_CACHE,
         )
         self.pipe.to("cuda")
+        # pytorch 2 optimization
+        self.pipe.unet = torch.compile(self.pipe.unet, mode="reduce-overhead", fullgraph=True)
 
     # Define the arguments and types the model takes as input
     def predict(
         self,
-        prompt: str = "full body,Cyber goth Geisha in the rain in a  tokyo future  city city wide, Pretty Face, Beautiful eyes",
+        prompt: str = "highly detailed portrait of an underwater city, with towering spires and domes rising up from the ocean",
         negative_prompt: str = "",
         steps: int = Input(description=" # Inference Steps", ge=0, le=100, default=50),
         guidance: float = Input(description="Guidance scale", default=9),
+        seed: int = Input(description="Seed (0 = random, maximum: 2147483647)", default=0),
     ) -> Path:
         """Run a single prediction on the model"""
+        if seed == 0:
+            seed = int.from_bytes(os.urandom(2), byteorder='big')
+        generator = torch.Generator('cuda').manual_seed(seed)
+
         image = self.pipe(
             prompt=prompt,
             negative_prompt=negative_prompt,
             num_inference_steps=steps,
             guidance_scale=guidance,
+            generator=generator
         ).images[0]
 
         gc.collect()
